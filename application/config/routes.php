267ef2d5ec4adf4195e94ce660d6ecc82cb9d455
adamz <58021562+ad4mny@users.noreply.github.com>
2022-01-13 01:32:13 +0800

    added login and registration form submit routes

--- application/config/routes.php
+++ application/config/routes.php
@@ -7,3 +7,6 @@ $route['translate_uri_dashes'] = FALSE;
 
 $route['login'] = 'LoginController/index/login';
 $route['register'] = 'LoginController/index/register';
+
+$route['login/submit'] = 'LoginController/login';
+$route['register/submit'] = 'LoginController/register';
