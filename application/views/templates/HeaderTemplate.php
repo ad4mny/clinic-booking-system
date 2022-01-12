55feb021f02c978e8836c05abc22b435e62e0b71
adamz <58021562+ad4mny@users.noreply.github.com>
2022-01-13 01:38:52 +0800

    added favicon

--- application/views/templates/HeaderTemplate.php
+++ application/views/templates/HeaderTemplate.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="shortcut icon" href="<?php echo base_url() ?>assets/img/favicon.ico" type="image/x-icon" />
+    <link rel="shortcut icon" href="<?php echo base_url() ?>assets/image/favicon.ico" type="image/x-icon" />
     <title>Health: Clinic Booking</title>
     <link rel="stylesheet" href="<?php echo base_url(); ?>assets/css/bootstrap.min.css">
     <link rel="stylesheet" href="<?php echo base_url(); ?>assets/css/custom-bootstrap.css">
@@ -20,12 +20,12 @@
     <div class="w-auto position-absolute start-50 translate-middle mt-5">
         <?php
         if ($this->session->tempdata('notice') != NULL) {
-            echo '<div class="alert alert-light shadow-sm alert-dismissible fade show" role="alert">';
+            echo '<div class="alert alert-light rounded-3 shadow-sm alert-dismissible fade show" role="alert">';
             echo '<p class="mb-0 text-success"><i class="fas fa-info-circle fa-fw fa-sm me-1"></i> ' . $this->session->tempdata('notice') . '</p>';
             echo '</div>';
         }
         if ($this->session->tempdata('error') != NULL) {
-            echo '<div class="alert alert-light shadow-sm alert-dismissible fade show" role="alert">';
+            echo '<div class="alert alert-light rounded-3 shadow-sm alert-dismissible fade show" role="alert">';
             echo '<p class="mb-0 text-danger"><i class="fas fa-exclamation-circle fa-fw fa-sm me-1"></i> ' . $this->session->tempdata('error') . '</p>';
             echo '</div>';
         }
